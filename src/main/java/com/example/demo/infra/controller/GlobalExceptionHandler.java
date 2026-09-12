31c68b029e2d06364c427054124505d09ab50dee
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-09-12 10:04:03 -0300

    Update GlobalExceptionHandler
    Now is refactored to use ApiResponse and handle DTO unexpected errors

--- src/main/java/com/example/demo/infra/controller/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/infra/controller/GlobalExceptionHandler.java
@@ -2,43 +2,61 @@ package com.example.demo.infra.controller;
 
 import com.example.demo.domain.exceptions.DomainException;
 import com.example.demo.domain.exceptions.ResourceNotFoundException;
+import com.example.demo.shared.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.Arrays;
+import java.util.stream.Collectors;
 
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<String> handleResourceNotFound(
+    public ResponseEntity<ApiResponse<Object>> handleResourceNotFound(
             ResourceNotFoundException exception
     ) {
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
-                .body(exception.getMessage());
+                .body(ApiResponse.error(exception.getMessage()));
     }
 
     @ExceptionHandler(DomainException.class)
-    public ResponseEntity<String> handleDomainException(
+    public ResponseEntity<ApiResponse<Object>> handleDomainException(
             DomainException exception
     ) {
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
-                .body(exception.getMessage());
+                .body(ApiResponse.error(exception.getMessage()));
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<String> handleValidation(MethodArgumentNotValidException exception) {
+    public ResponseEntity<ApiResponse<Object>> handleValidation(
+            MethodArgumentNotValidException exception
+    ) {
 
-        String message = exception.getMessage();
+        String message = exception.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .collect(Collectors.joining(","));
+
 
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
-                .body(message.isEmpty() ? "Invalid request" : message));
+                .body(ApiResponse.error(message));
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ApiResponse<Object>> handleMessageNotReadable(
+            HttpMessageNotReadableException exception
+    ) {
+        return ResponseEntity
+                .badRequest()
+                .body(ApiResponse.error("Invalid request body"));
     }
 
 
