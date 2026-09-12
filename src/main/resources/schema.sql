5d542be48d26bf7221098a2930731acf18796884
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-09-12 08:56:17 -0300

    Change schema to verify table existence

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,13 +1,13 @@
 CREATE SCHEMA IF NOT EXISTS test_nekit;
 
-CREATE TABLE test_nekit.projects (
+CREATE TABLE IF NOT EXISTS test_nekit.projects (
     id UUID PRIMARY KEY DEFAULT uuidv7(),
     title VARCHAR(250) NOT NULL,
     description TEXT,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE test_nekit.tasks (
+CREATE TABLE IF NOT EXISTS test_nekit.tasks (
     id UUID PRIMARY KEY DEFAULT uuidv7(),
     project_id UUID NOT NULL,
     title VARCHAR(250) NOT NULL,
