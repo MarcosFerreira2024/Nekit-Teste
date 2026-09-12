bb1811cba9d398d310de8d67d46f788d67e37e37
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-09-12 13:47:28 -0300

    Create the FindTask useCase

--- src/main/java/com/example/demo/application/useCase/task/FindTask.java
+++ src/main/java/com/example/demo/application/useCase/task/FindTask.java
@@ -1,5 +1,35 @@
 package com.example.demo.application.useCase.task;
 
+import com.example.demo.application.dto.task.FindTaskDTO;
+import com.example.demo.domain.entity.Task;
+import com.example.demo.domain.enums.TaskPriority;
+import com.example.demo.domain.exceptions.DomainException;
+import com.example.demo.infra.repository.TaskFilterRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Locale;
+
+
+@Service
 public class FindTask {
-    
+
+    private final TaskFilterRepository taskRepository;
+
+    public FindTask(TaskFilterRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Task> execute(FindTaskDTO query){
+
+        try{
+            TaskPriority.valueOf(query.priority().toUpperCase(Locale.ROOT));
+
+        } catch (IllegalArgumentException e) {
+            throw new DomainException("Wrong priority usage");
+        }
+        return taskRepository.findByQuery(query);
+
+    }
+
 }
