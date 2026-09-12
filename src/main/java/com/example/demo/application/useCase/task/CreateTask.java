a0b850071c00cb3210ce14edb202ce73a2571045
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-09-12 10:50:06 -0300

    Create a Validation for projectId existence

--- src/main/java/com/example/demo/application/useCase/task/CreateTask.java
+++ src/main/java/com/example/demo/application/useCase/task/CreateTask.java
@@ -1,20 +1,30 @@
 package com.example.demo.application.useCase.task;
 
 import com.example.demo.application.dto.task.CreateTaskDTO;
+import com.example.demo.domain.entity.Project;
 import com.example.demo.domain.entity.Task;
+import com.example.demo.domain.exceptions.ResourceNotFoundException;
+import com.example.demo.infra.repository.ProjectRepository;
 import com.example.demo.infra.repository.TaskRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CreateTask {
 
     private final TaskRepository taskRepository;
+    private final ProjectRepository projectRepository;
 
-    public CreateTask(TaskRepository taskRepository) {
+    public CreateTask(TaskRepository taskRepository, ProjectRepository projectRepository) {
         this.taskRepository = taskRepository;
+        this.projectRepository = projectRepository;
     }
 
     public Task execute(CreateTaskDTO dto) {
+
+
+        projectRepository.findById(dto.projectId()).orElseThrow(() -> new ResourceNotFoundException("ProjectId",dto.projectId()));
 
         Task task = new Task(dto);
         return taskRepository.save(task);
