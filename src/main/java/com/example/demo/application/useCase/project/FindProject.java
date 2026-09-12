a601fd0d551aa62194e1deacaec2c25b094931be
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-09-12 11:45:49 -0300

    Create a Query Based useCase for Project

--- src/main/java/com/example/demo/application/useCase/project/FindProject.java
+++ src/main/java/com/example/demo/application/useCase/project/FindProject.java
@@ -1,4 +1,26 @@
 package com.example.demo.application.useCase.project;
+
+import com.example.demo.application.dto.project.FindProjectDTO;
+import com.example.demo.domain.entity.Project;
+import com.example.demo.infra.repository.ProjectFilterRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
 public class FindProject {
-    
+
+    private final ProjectFilterRepository projectRepository;
+
+    public FindProject(ProjectFilterRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
+
+    public List<Project> execute(FindProjectDTO query){
+
+        return projectRepository.findByQuery(query);
+
+    }
+
 }
