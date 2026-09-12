8451d92067a97e153471c1bd010e1f5f4f5509c0
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-09-12 10:01:09 -0300

    Update ApiResponse to use ToString / minor refactor

--- src/main/java/com/example/demo/shared/ApiResponse.java
+++ src/main/java/com/example/demo/shared/ApiResponse.java
@@ -1,8 +1,10 @@
 package com.example.demo.shared;
 
+import java.util.Arrays;
+
 public record ApiResponse<T>(
         Boolean success,
-        T Data,
+        T data,
         String message
 ) {
 
@@ -14,7 +16,21 @@ public record ApiResponse<T>(
         return new ApiResponse<>(true, data, message);
     }
 
+    public static <T> ApiResponse<T> success(String message) {
+        return new ApiResponse<>(true, null, message);
+    }
+
     public static <T> ApiResponse<T> error(String message) {
         return new ApiResponse<>(false, null, message);
+    }
+
+
+    @Override
+    public String toString() {
+        return "{" +
+                "success=" + success +
+                ", data=" + data +
+                ", message='" + message + '\'' +
+                '}';
     }
 }
