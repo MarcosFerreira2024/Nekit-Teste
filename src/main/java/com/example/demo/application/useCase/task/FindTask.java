a8cf2f6c840c52b345cc2ef15dcdc34774871ada
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-09-12 13:51:24 -0300

    Update FindTask.java

--- src/main/java/com/example/demo/application/useCase/task/FindTask.java
+++ src/main/java/com/example/demo/application/useCase/task/FindTask.java
@@ -23,7 +23,7 @@ public class FindTask {
     public List<Task> execute(FindTaskDTO query){
 
         try{
-            if(query.priority() != null) TaskPriority.valueOf(query.priority().toUpperCase(Locale.ROOT));
+            if(query.priority() != null && !query.priority().isBlank()) TaskPriority.valueOf(query.priority().toUpperCase(Locale.ROOT));
 
         } catch (IllegalArgumentException e) {
             throw new DomainException("Wrong priority usage");
