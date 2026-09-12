b4bb1fcdc2dff4e600cca01615a10d77b4bb6667
Marcos Vinicius Ferreira <marcosfp2021@hotmail.com>
2026-09-12 09:02:27 -0300

    Create a default api response

---
+++ src/main/java/com/example/demo/shared/ApiResponse.java
@@ -0,0 +1,20 @@
+package com.example.demo.shared;
+
+public record ApiResponse<T>(
+        Boolean success,
+        T Data,
+        String message
+) {
+
+    public static <T> ApiResponse<T> success(T data) {
+        return new ApiResponse<>(true, data, null);
+    }
+
+    public static <T> ApiResponse<T> success(T data, String message) {
+        return new ApiResponse<>(true, data, message);
+    }
+
+    public static <T> ApiResponse<T> error(String message) {
+        return new ApiResponse<>(false, null, message);
+    }
+}
